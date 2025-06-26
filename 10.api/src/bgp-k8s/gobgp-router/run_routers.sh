e138abcb352f8b72f65a12009d5570524de0d35b
majednitol <majedrahman7@gmail.com>
2025-06-27 00:03:32 +0600

    done

--- 10.api/src/bgp-k8s/gobgp-router/run_routers.sh
+++ 10.api/src/bgp-k8s/gobgp-router/run_routers.sh
@@ -15,25 +15,25 @@ gobgpd -f /gobgp/router3.conf \
   --api-hosts 0.0.0.0:50053 \
   --log-level debug > /dev/stdout 2>&1 &
 
-echo "Starting router4..."
-gobgpd -f /gobgp/router4.conf \
-  --api-hosts 0.0.0.0:50054 \
-  --log-level debug > /dev/stdout 2>&1 &  
+# echo "Starting router4..."
+# gobgpd -f /gobgp/router4.conf \
+#   --api-hosts 0.0.0.0:50054 \
+#   --log-level debug > /dev/stdout 2>&1 &  
 
-echo "Starting router5..."
-gobgpd -f /gobgp/router5.conf \
-  --api-hosts 0.0.0.0:50055 \
-  --log-level debug > /dev/stdout 2>&1 & 
+# echo "Starting router5..."
+# gobgpd -f /gobgp/router5.conf \
+#   --api-hosts 0.0.0.0:50055 \
+#   --log-level debug > /dev/stdout 2>&1 & 
 
-echo "Starting router6..."
-gobgpd -f /gobgp/router6.conf \
-  --api-hosts 0.0.0.0:50056 \
-  --log-level debug > /dev/stdout 2>&1 & 
+# echo "Starting router6..."
+# gobgpd -f /gobgp/router6.conf \
+#   --api-hosts 0.0.0.0:50056 \
+#   --log-level debug > /dev/stdout 2>&1 & 
 
-echo "Starting router7..."
-gobgpd -f /gobgp/router7.conf \
-  --api-hosts 0.0.0.0:50057 \
-  --log-level debug > /dev/stdout 2>&1 & 
+# echo "Starting router7..."
+# gobgpd -f /gobgp/router7.conf \
+#   --api-hosts 0.0.0.0:50057 \
+#   --log-level debug > /dev/stdout 2>&1 & 
 
 sleep 2
 echo "Routers are running with debug logs:"
