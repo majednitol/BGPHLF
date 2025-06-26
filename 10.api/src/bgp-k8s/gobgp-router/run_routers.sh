127115f21f85d4c6f4c340e720879b606730290b
majednitol <majedrahman7@gmail.com>
2025-06-26 22:33:10 +0600

    done

--- 10.api/src/bgp-k8s/gobgp-router/run_routers.sh
+++ 10.api/src/bgp-k8s/gobgp-router/run_routers.sh
@@ -5,16 +5,16 @@ gobgpd -f /gobgp/router1.conf \
   --api-hosts 0.0.0.0:50051 \
   --log-level debug > /dev/stdout 2>&1 &
 
-# echo "Starting router2..."
-# gobgpd -f /gobgp/router2.conf \
-#   --api-hosts 0.0.0.0:50052 \
-#   --log-level debug > /dev/stdout 2>&1 &
+echo "Starting router2..."
+gobgpd -f /gobgp/router2.conf \
+  --api-hosts 0.0.0.0:50052 \
+  --log-level debug > /dev/stdout 2>&1 &
 
 echo "Starting router3..."
 gobgpd -f /gobgp/router3.conf \
   --api-hosts 0.0.0.0:50053 \
   --log-level debug > /dev/stdout 2>&1 &
-
+  
 sleep 2
 echo "Routers are running with debug logs:"
 echo "-----------------------------------------"
