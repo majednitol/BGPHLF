801b99ed9583e3b7dfca7dce11d8a56482ffcd7c
majednitol <majedrahman7@gmail.com>
2025-06-30 14:29:46 +0600

    done

--- 10.api/src/app.js
+++ 10.api/src/app.js
@@ -7,6 +7,7 @@ import gobalErrorHander from "./middleware/gobalErrorHander.js";
 
 import ipPrefixRouter from "./router/ipPrefixRouter.js";
 import companyRouter from "./router/companyRouter.js";
+import { scheduleRIRJob } from "./services/CronJob.js";
 app.use(cors())
 app.use(express.json());
 
@@ -14,7 +15,7 @@ app.use('/ip', ipPrefixRouter);
 app.use("/user", userRouter)
 app.use("/company", companyRouter)
 app.use(gobalErrorHander)
-
+scheduleRIRJob();
 app.listen(4000, () => {
     console.log("server started");
 
