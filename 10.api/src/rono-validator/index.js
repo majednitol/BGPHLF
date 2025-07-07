9b183cf14375d18aee2a4c40a8c0965f7ceaa82f
majednitol <majedrahman7@gmail.com>
2025-07-07 22:27:07 +0600

    done

--- 10.api/src/rono-validator/index.js
+++ 10.api/src/rono-validator/index.js
@@ -66,12 +66,13 @@ async function refreshROAs() {
 
   try {
     const { data } = await axios.get(`${API_BASE}/ip/get-all-as-data`);
-
-    // Flatten and validate each prefix individually
+    console.log("data",data)
     for (const entry of data) {
       const asnNum = parseInt(entry.asn.replace("AS", "").trim());
-
+console.log("asnNum",asnNum)
       for (const prefix of entry.prefix) {
+        console.log("prefix", prefix)
+        console.log("entry.prefix", entry.prefix)
         try {
           const res = await axios.get(`${API_BASE}/ip/trace-prefix`, {
             params: { prefix, asn: asnNum }
